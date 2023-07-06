356b7cdc3fdf86922489a8a369667e02e917f44e
1F10220064 <s1f102200648@iniad.org>
2023-07-06 14:03:24 +0900

    警告の修正1

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.utils.timezone import make_aware
-from django.utils.dateparse import parse_datetime 
+from django.utils.dateparse import parse_datetime
 from todo.models import Task
+
 
 # Create your views here.
 def index(request):
@@ -18,3 +19,4 @@ def index(request):
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
+    
