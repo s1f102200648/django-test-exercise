445394ba69f691f8abf8fda375c84256b5644442
1F10220064 <s1f102200648@iniad.org>
2023-07-06 14:06:29 +0900

    警告の修正2

--- todo/tests.py
+++ todo/tests.py
@@ -54,6 +54,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase (TestCase):
     def test_index_get(self):
         client = Client()
@@ -97,3 +98,4 @@ class TodoViewTestCase (TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
+        
--- todo/views.py
+++ todo/views.py
@@ -18,5 +18,5 @@ def index(request):
     context = {
         'tasks': tasks
     }
-    return render(request, 'todo/index.html', context)
     
+    return render(request, 'todo/index.html', context)
